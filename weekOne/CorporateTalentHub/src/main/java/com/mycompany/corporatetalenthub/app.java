36066cfc87611f35d83b74f4af39a194b5504dc8
cohorte_5 <cohorte_5@rwadmin-90533010005>
2026-07-30 19:53:09 -0500

    add the proyect's structure

---
+++ weekOne/CorporateTalentHub/src/main/java/com/mycompany/corporatetalenthub/app.java
@@ -0,0 +1,14 @@
+package com.mycompany.corporatetalenthub;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author cohorte_5
+ */
+public class app {
+
+    public static void main(String[] args) {
+        
+        
+        
+    }
+}
